6a53c4c58251c7a5598463770763352a7c8e3e79
santakratos <santakratos10@gmail.com>
2022-06-13 22:02:04 +0300

    v1.0

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@
  * @param {string} string
  * @returns {string}
  */
-export const capitalizeString = 0;
+export const capitalizeString = (string) => string.split(' ').map((word) => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`).join(' ');
 
 /**
  * Должна быть function declaration
  * @param {string} string
  * @returns {string}
  */
-export const fenceString = 0;
+export function fenceString (string) {
+  let rewriteString = '';
+  for (let i = 0; i < string.length; i++) {
+    if (i % 2 === 1) {
+      rewriteString = `${rewriteString}${string[i].toUpperCase()}`;
+    } else {
+      rewriteString = `${rewriteString}${string[i].toLowerCase()}`;
+    }
+  }
+  return rewriteString;
+}
 
 /**
  * Должна быть function expression
@@ -18,7 +28,27 @@ export const fenceString = 0;
  * @param {string} string
  * @returns {string}
  */
-export const reducerIf = 0;
+export const reducerIf = function(action, string) {
+  if (action === 'uppercase') {
+    return string.toUpperCase();
+  } else if (action === 'lowercase') {
+    return string.toLowerCase();
+  } else if (action === 'fence') {
+    let rewriteString = '';
+    for (let i = 0; i < string.length; i++) {
+      if (i % 2 === 1) {
+        rewriteString = `${rewriteString}${string[i].toUpperCase()}`;
+      } else {
+        rewriteString = `${rewriteString}${string[i].toLowerCase()}`;
+      }
+    }
+    return rewriteString;
+  } else if (action === 'capitalize') {
+    return string.split(' ').map((word) => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`).join(' ');
+  } else {
+    return string;
+  }
+};
 
 /**
  * Стрелочная
@@ -26,28 +56,65 @@ export const reducerIf = 0;
  * @param {string} string
  * @returns {string}
  */
-export const reducerSwitch = 0;
+export const reducerSwitch = (action, string) => {
+  switch (action) {
+    case 'uppercase':
+      return string.toUpperCase();
+    case 'lowercase':
+      return string.toLowerCase();
+    case 'fence':
+      let rewriteString = '';
+      for (let i = 0; i < string.length; i++) {
+        if (i % 2 === 1) {
+          rewriteString = `${rewriteString}${string[i].toUpperCase()}`;
+        } else {
+          rewriteString = `${rewriteString}${string[i].toLowerCase()}`;
+        }
+      }
+      return rewriteString;
+    case 'capitalize':
+      return string.split(' ').map((word) => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`).join(' ');
+    default:
+      return string;
+  }
+};
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsForOf = 0;
+export const consoleLoggerWordsForOf = (string) => {
+  for (let letter of string) {
+    console.log(letter)
+  }
+};
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsFor = 0;
+export const consoleLoggerWordsFor = (string) => {
+  for (let i = 0; i < string.length; i++) {
+    console.log(string[i])
+  }
+};
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsWhile = 0;
+export const consoleLoggerWordsWhile = (string) => {
+  let i = 0;
+  while (i < string.length) {
+    console.log(string[i])
+    ++i;
+  }
+};
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsSplit = 0;
+export const consoleLoggerWordsSplit = (string) => {
+  string.split('').map ((letter) => console.log(letter))
+};
