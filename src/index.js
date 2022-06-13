9cb8ff92dfa9ea4395a1dcad48d2c84b31b9794d
santakratos <santakratos10@gmail.com>
2022-06-13 22:18:59 +0300

    v1.3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  * @param {string} string
  * @returns {string}
  */
-export const capitalizeString = (string) => string.toLowerCase().split(' ').map((word) => `${word[0].toUpperCase()}${word.slice(1)}`).join(' ');
+export const capitalizeString = (string) => string.split(' ').map((word) => `${word[0].toUpperCase()}${word.slice(1)}`).join(' ');
 
 /**
  * Должна быть function declaration
